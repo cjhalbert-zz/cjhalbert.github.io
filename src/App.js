254f111463e396c7e5122b1840070ccb9d3a4505
Corey Halbert <coreyhalbert@Coreys-MacBook-Pro.local>
2019-01-16 15:26:27 -0500

    Add react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./sass/stylesheet.scss";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>What's up</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
