4b84f96e0a7e79a696c466ca9f08553522c76a43
Corey Halbert <coreyhalbert@Coreys-MacBook-Pro.local>
2019-01-18 10:24:17 -0500

    Style header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import "./sass/stylesheet.scss";
 
+import Header from './components/Header.js';
+import Splash from './components/Splash.js';
+import Projects from './components/Projects.js';
+import Contact from './components/Contact.js';
+import About from './components/About.js';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>What's up</h1>
+        <Header />
+        <Splash />
+        <Projects />
+        <Contact />
+        <About />
       </div>
     );
   }
