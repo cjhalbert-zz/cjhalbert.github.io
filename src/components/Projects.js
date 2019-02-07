f2b25e2519ca3b2e31cd428a0ba6b511da0ec0ed
Corey Halbert <coreyhalbert@Coreys-MacBook-Pro.local>
2019-02-06 23:55:25 -0500

    Style work page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,8 +3,41 @@ import React, { Component } from 'react';
 class Projects extends Component {
     render() {
         return (
-            <main>
+            <main className="work">
                 <h1 className="heading-1">work</h1>
+                <section className="work-section">
+                    <ul className="projects-list">
+                        <li>
+                            <p>digital illustration</p>
+                            <h2>36 DAYS OF TYPE</h2>
+                        </li>
+                        <li>
+                            <p>web design</p>
+                            <h2>BENEFICENCE RECORDS</h2>
+                        </li>
+                        <li>
+                            <p>digital illustration</p>
+                            <h2>SHOW POSTERS</h2>
+                        </li>
+                        <li>
+                            <p>digital illustration</p>
+                            <h2>WHOCARES VISUAL STYLE</h2>
+                        </li>
+                        <li>
+                            <p>web design</p>
+                            <h2>INNOVATIVE EDUCATORS CONFERENCE</h2>
+                        </li>
+                        <li>
+                            <p>branding</p>
+                            <h2>MAXWELL DENARI</h2>
+                        </li>
+                        <li>
+                            <p>branding</p>
+                            <h2>REST IN PEACH</h2>
+                        </li>
+                    </ul>
+                </section>
+                <div className="project-preview"></div>
             </main>
         )
     }
